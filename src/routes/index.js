f9aadae16dffef7453e35487bb61a0b0a36a4b0c
Phan An Khang <minecraftcopyright1302@gmail.com>
2025-03-05 22:17:33 +0700

    l;fd

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 import Dashboard from "../pages/admin/DashboardPage/Dashboard";
+import CartPage from "../pages/customer/CartPage/CartPage";
+import FavoritePage from "../pages/customer/FavoritePage/FavoritePage";
 import HomePage from "../pages/customer/HomePage/HomePage";
+import NewPage from "../pages/customer/NewPage/NewPage";
 import NotFoundPage from "../pages/customer/NotFoundPage/NotFoundPage";
 import OrderPage from "../pages/customer/OrderPage/OrderPage";
 import ProductsPage from "../pages/customer/ProductsPage/ProductsPage";
+import PromotionPage from "../pages/customer/PromotionPage/PromotionPage";
 
 export const routes = [
     // customer
@@ -21,6 +25,30 @@ export const routes = [
     {
         path: '/products',
         page: ProductsPage,
+        isShowHeader: true,
+        isShowFooter: true
+    },
+    {
+        path: '/promotion',
+        page: PromotionPage,
+        isShowHeader: true,
+        isShowFooter: true
+    },
+    {
+        path: '/new',
+        page: NewPage,
+        isShowHeader: true,
+        isShowFooter: true
+    },
+    {
+        path: '/cart',
+        page: CartPage,
+        isShowHeader: true,
+        isShowFooter: true
+    },
+    {
+        path: '/favorite',
+        page: FavoritePage,
         isShowHeader: true,
         isShowFooter: true
     },
